7563d58e50d6f25be3e05803a0740c554e66b135
gehrig rankin <rgrankin22@gmail.com>
2018-08-22 14:13:53 -0700

    routing is done

---
+++ client/src/pages/NoMatch/NoMatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NoMatch extends Component {
+    render(){
+        return (
+            <div className="NoMatch">
+                <p>no match</p>
+            </div>
+        )
+    }
+}
+
+export default NoMatch;
